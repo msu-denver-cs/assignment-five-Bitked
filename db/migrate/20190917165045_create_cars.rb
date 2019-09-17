9eac43ff7d63e34f70a7c44d8e42e214efc213cd
Bitked <birt011e@gmail.com>
2019-09-17 11:08:50 -0600

    had to use integer not int for cars scaffold to work properly

--- db/migrate/20190917165045_create_cars.rb
+++ db/migrate/20190917165045_create_cars.rb
@@ -4,7 +4,7 @@ class CreateCars < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :model
       t.string :country
-      t.int :vin
+      t.integer :vin
 
       t.timestamps
     end
