2835874425486024e79664f87c8d8d40526be924
Bitked <birt011e@gmail.com>
2019-10-05 16:41:25 -0600

    models tests most added but only part is successfull

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,11 +2,9 @@ class Car < ApplicationRecord
   belongs_to :make
   has_and_belongs_to_many :parts
 
-
-  validates :make, presence: true
   validates_associated :parts
+  validates :make, presence: true
   validates :model, presence: true, length: { in: 2..16 }
   validates :vin, presence: true, uniqueness: true, 
     format: { with: /[0-9]/}, length: { is: 6 }, numericality: { only_integer: true }
-
 end
--- test/models/part_test.rb
+++ test/models/part_test.rb
@@ -4,4 +4,21 @@ class PartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "for one character name" do
+    s = Part.create({:part => "a"})
+    refute s.valid?
+    refute s.save
+    assert_equal({:part=>["is too short (minimum is 2 characters)"]},
+      s.errors.messages)
+  end
+
+  test "for two character name" do
+    s = Part.create({:part => "aa"})
+    assert s.valid?
+    assert s.save
+    assert_equal({}, s.errors.messages)
+  end
+
+
 end
