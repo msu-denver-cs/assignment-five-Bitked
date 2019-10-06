5b4bf62811195ccf622899cc44e50563beb95b45
Bitked <birt011e@gmail.com>
2019-10-06 14:47:46 -0600

    clean up need auto

--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,21 +1,4 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  # test "for two character model" do
-  #   s = Car.create({:model => "aa"})
-  #   assert s.valid?
-  #   assert s.save
-  #   assert_equal({}, s.errors.messages)
-  # end
-
-  # test "for 6 character vin" do
-  #   s = Car.create({:vin => "123456"})
-  #   assert s.valid?
-  #   assert s.save
-  #   assert_equal({}, s.errors.messages)
-  # end
 end
